67824011c20327bf88a7768189cc15fbe9fe0f82
Luka <luka.lukas159@gmail.com>
2025-04-26 14:09:59 +0200

    initial commit

---
+++ src/main/java/com/luka/runnableExamples/derby/Main.java
@@ -0,0 +1,5 @@
+package com.luka.runnableExamples.derby;
+
+public class Main {
+    String url = "jdbc:derby:testdb;create=true";
+}
---
+++ src/main/java/com/luka/runnableExamples/simpledb/Main.java
@@ -0,0 +1,4 @@
+package com.luka.runnableExamples.simpledb;
+
+public class Main {
+}
